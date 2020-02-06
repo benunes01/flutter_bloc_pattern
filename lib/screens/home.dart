0e7c839515418a3cdc852c8be5ba544258334eda
benunes01 <be_nunes_pinheiro@hotmail.com>
2020-02-06 15:42:41 -0300

    Criação do sistema de busca, indicando as sugestões quando escreve algo na barra de pesquisa

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_pattern/delegates/data_search.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -23,7 +24,9 @@ class Home extends StatelessWidget {
           ),
           IconButton(
             icon: Icon(Icons.search),
-            onPressed: () {},
+            onPressed: () {
+              showSearch(context: context, delegate: DataSearch());
+            },
           ),
         ],
       ),
