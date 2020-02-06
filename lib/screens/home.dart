84aca77af3955620589927f8b9d8508de7ddc0e6
benunes01 <be_nunes_pinheiro@hotmail.com>
2020-02-06 11:29:12 -0300

    criação da pagina Home com o App bar de pesquisa

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,7 +4,30 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return null;
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 100,
+          child: Image.asset("images/youtube_PNG13.png"),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black,
+        actions: <Widget>[
+          Align(
+            alignment: Alignment.center,
+            child: Text("0"),
+          ),
+          IconButton(
+            icon: Icon(Icons.star),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Container(),
+    );
   }
-
 }
